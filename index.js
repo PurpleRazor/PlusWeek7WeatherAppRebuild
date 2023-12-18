2536a468b5ed80c8d5459286f33b898a9f34bcff
JennyStyles <97371372+PurpleRazor@users.noreply.github.com>
2023-12-18 19:24:14 +0000

    adding search engine function and testing progress

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-alert("hey bro!");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let seachInput = documet.querySelector("#search-input");
+  console.log(seachInput.value);
+}
+
+let searchFormElement = document.querySelector("#city-search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
