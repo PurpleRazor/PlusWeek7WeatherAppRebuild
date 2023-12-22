b2f1f3a3a502b207a6bc161d78b44a4ba23ae2e7
JennyStyles <97371372+PurpleRazor@users.noreply.github.com>
2023-12-22 00:35:05 +0000

    Live forecast data injected (Includes a bug that is preventing weather icons from displaying)

--- index.js
+++ index.js
@@ -61,6 +61,13 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#city-search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tes", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "a5dd6f056bd100c144634b3tof095574";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -68,28 +75,36 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response);
+  console.log(response.data);
 
-  let days = ["Tue", "Weds", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
             <div class="weather-forecast-day">
-              <div class="weather-forecast-date">${day}</div>
-              <div class="weather-forecast-icon">🌦</div>
+              <div class="weather-forecast-date">${formatDay(day.time)}</div>
+              <div >
+              <img scr="${
+                day.condition.icon_url
+              }" class="weather-forecast-icon" />
+              </div>
               <div class="weather-forecast-temperatures">
-                <span class="weather-forecast-temperature-max">18</span>
-                <span class="weather-forecast-temperature-min">12</span>
+                <span class="weather-forecast-temperature-max">${Math.round(
+                  day.temperature.maximum
+                )}</span>
+                <span class="weather-forecast-temperature-min">${Math.round(
+                  day.temperature.minimum
+                )}</span>
               </div>
             </div>
           `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
 
 searchCity("London");
-displayForecast();
