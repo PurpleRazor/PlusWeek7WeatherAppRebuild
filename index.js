51d9c91e0a7b5f2b472f3c0eac9a7548cfd484c4
JennyStyles <97371372+PurpleRazor@users.noreply.github.com>
2023-12-22 21:44:05 +0000

    Bug fix for forecast emojis and tweaking CSS for mobile display

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ function displayForecast(response) {
             <div class="weather-forecast-day">
               <div class="weather-forecast-date">${formatDay(day.time)}</div>
               <div >
-              <img scr="${
+              <img src="${
                 day.condition.icon_url
               }" class="weather-forecast-icon" />
               </div>
